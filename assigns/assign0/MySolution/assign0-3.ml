9ed257a4190a9c17f44c8db2ddb7bfaea8df16fb
arionct <arionct@bu.edu>
2023-09-20 17:54:21 -0400

    update

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,11 +1,11 @@
-#use "./../assign0.ml"
+#use "./../assign0.ml";;
 
 let int2str(i0: int): string =
   let rec helper i acc =
     if i = 0 then
       acc
     else
-      let digit = abs (i mod 10) in
+      let digit = i mod 10 in
       let char_digit = chr (digit + 48) in
       helper (i / 10) (str char_digit ^ acc)
   in
