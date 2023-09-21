739f3f6c6b4386850db063f6979a3d5e788da0ca
arionct <arionct@bu.edu>
2023-09-20 21:26:35 -0400

    update

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,6 +1,15 @@
 #use "./../assign0.ml";;
 #use "./../../../classlib/OCaml/MyOCaml.ml";;
 
+let char_string_concat (c: char) (s: string) : string =
+  let len = string_length s in
+  string_init (len + 1) (fun i ->
+    if i = 0 then
+      chr (ord c)
+    else
+      string_get (s, i - 1)
+  )
+
 let int2str(i0: int): string =
   let rec helper i acc =
     if i = 0 then
@@ -8,8 +17,8 @@ let int2str(i0: int): string =
     else
       let digit = i mod 10 in
       let char_digit = chr (digit + 48) in
-      helper (i / 10) (string_cons char_digit acc)
+      helper (i / 10) (char_string_concat char_digit acc)
   in
   if i0 = 0 then "0" else
-  if i0 < 0 then string_cons '-' (helper (abs i0) "")
+  if i0 < 0 then char_string_concat '-' (helper (abs i0) "")
   else helper i0 ""
