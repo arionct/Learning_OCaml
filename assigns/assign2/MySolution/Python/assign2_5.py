b194379aada7b68391912470b6e2435699ed3064
arionct <arionct@bu.edu>
2023-09-27 22:26:18 -0400

    update

--- assigns/assign2/MySolution/Python/assign2_5.py
+++ assigns/assign2/MySolution/Python/assign2_5.py
@@ -2,6 +2,6 @@ def fnlist_make_fwork(fopr):
     def fn(xs):
         acc = []
         for x in xs:
-            acc.insert(0, fopr(x))
+            acc.append(fopr(x))
         return acc
     return fn
