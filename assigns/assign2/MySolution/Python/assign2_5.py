7de10546837f9880bff60eed1a216c6f43ac2b3a
arionct <arionct@bu.edu>
2023-09-27 23:28:27 -0400

    update

--- assigns/assign2/MySolution/Python/assign2_5.py
+++ assigns/assign2/MySolution/Python/assign2_5.py
@@ -2,9 +2,19 @@ import sys
 sys.path.append("./../../../../classlib/Python")
 
 def fnlist_make_fwork(fopr):
-  acc = []
+  #acc = []
   def fn(xs):
-    acc.append(xs)
-    fopr(fn)
-    return acc
+    newxs = fopr(xs)
+    #acc.append(newxs)
+    #fopr(xs)
+    return newxs
   return fn
+
+
+def double(x):
+    return 2 * x
+
+mylist = [1, 2, 3, 4, 5]
+myfn = fnlist_make_fwork(double)
+result = myfn(mylist)
+print(result)  # Output: [2, 4, 6, 8, 10]
--- assigns/assign2/MySolution/Python/assign2_6.py
+++ assigns/assign2/MySolution/Python/assign2_6.py
@@ -2,14 +2,14 @@ import sys
 sys.path.append("./../../../../classlib/Python")
 
 def string_merge(cs1, cs2):
-    n1 = len(cs1)
-    n2 = len(cs2)
-
+    n1 = string_length(cs1)
+    n2 = string_length(cs2)
+    
     def foreach(i1, i2, work):
         if i1 < n1:
             if i2 < n2:
-                c1 = cs1[i1]
-                c2 = cs2[i2]
+                c1 = string_get_at(cs1, i1)
+                c2 = string_get_at(cs2, i2)
                 if c1 <= c2:
                     work(c1)
                     foreach(i1 + 1, i2, work)
@@ -17,11 +17,11 @@ def string_merge(cs1, cs2):
                     work(c2)
                     foreach(i1, i2 + 1, work)
             else:
-                for i in range(n1 - i1):
-                    work(cs1[i1 + i])
+                for iter in range(i1, n1):
+                    work(cs1[iter])
         else:
-            for i in range(n2 - i2):
-                work(cs2[i2 + i])
+            for iter in range(i2, n2):
+                work(cs2[iter])
 
     return ''.join(foreach(0, 0, lambda x: [x]))
 
