8782ebf472e08f40b1e82f1af2125b675778dccc
arionct <arionct@bu.edu>
2023-10-03 12:18:47 -0400

    update

--- assigns/assign3/MySolution/assign3-1.ml
+++ assigns/assign3/MySolution/assign3-1.ml
@@ -3,10 +3,10 @@
 
 let rec matrix_transpose(xss: 'a list list): 'a list list =
   match xss with
-  | [] -> []
-  | [] :: _ -> []
+  | [] -> [] (* If the matrix is empty, return an empty matrix *)
+  | [] :: _ -> [] (* If any of the inner lists is empty, return an empty matrix *)
   | _ ->
-    let head_column = List.map List.hd xss in
-    let tail_transposed = matrix_transpose (List.map List.tl xss) in
-    head_column :: tail_transposed
+    let head_column = foreach_to_listize(list_foreach)(xss)(fun xs -> List.hd xs) in
+    let tail_matrix = foreach_to_listize(list_foreach)(xss)(fun xs -> List.tl xs) in
+    head_column :: matrix_transpose(tail_matrix)
 ;;
