9a86ed4ffe39f5f08142aafd6a72800ab68b0104
arionct <arionct@bu.edu>
2023-12-04 23:05:54 -0500

    update

--- assigns/interp2/MySolution/interp2.ml
+++ assigns/interp2/MySolution/interp2.ml
@@ -13,27 +13,32 @@ Notes:
 *)
 
 (* abstract syntax tree of interp1 *)
+
 type const =
   | Int of int
   | Bool of bool
   | Unit
   | Char of char
   | Sym of string
+  | Closure of string * env * coms
 
-type com =
+and env = (string * const) list
+
+and com =
   | Push of const | Pop | Swap | Trace
   | Add | Sub | Mul | Div
   | And | Or | Not
   | Lt | Gt
   | If of coms * coms
   | Bind | Lookup
-  | Fun of coms | Call | Return
+  | Fun of string * coms | Call | Return
 
 and coms = com list
 
 (* ------------------------------------------------------------ *)
 
 (* parsers for interp1 *)
+
 let parse_nat = 
   let* n = natural << whitespaces in pure n
 
@@ -56,7 +61,6 @@ let parse_char =
 let is_sym_char c = 
    (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')
    
-
 let string_of_char_list char_list =
    String.concat "" (List.map (String.make 1) char_list)
     
@@ -64,7 +68,6 @@ let parse_sym =
    let* sym_chars = many1 (satisfy is_sym_char) << whitespaces in
    pure (Sym (string_of_char_list sym_chars))
     
- 
 let parse_const =
   parse_int <|>
   parse_bool <|>
@@ -72,9 +75,7 @@ let parse_const =
   parse_char <|>
   parse_sym
 
-
-
-  let parse_if_else parse_coms =
+let parse_if_else parse_coms =
    let* _ = keyword "If" in
    let* if_branch = parse_coms () in
    let* _ = keyword "Else" in
@@ -82,13 +83,19 @@ let parse_const =
    let* _ = keyword "End" in
    pure (If (if_branch, else_branch))
  
- let parse_fun parse_coms =
+let parse_sym_as_string =
+   let* sym_chars = many1 (satisfy is_sym_char) << whitespaces in
+   pure (string_of_char_list sym_chars)
+   
+let parse_fun parse_coms =
    let* _ = keyword "Fun" in
+   let* sym = parse_sym_as_string in
    let* fun_body = parse_coms () in
    let* _ = keyword "End" in
-   pure (Fun fun_body)
+   pure (Fun (sym, fun_body))
+       
  
- let parse_com parse_coms = 
+let parse_com parse_coms = 
    (keyword "Push" >> parse_const >>= fun c -> pure (Push c)) <|>
    (keyword "Pop" >> pure Pop) <|>
    (keyword "Swap" >> pure Swap) <|>
@@ -109,7 +116,7 @@ let parse_const =
    (keyword "Call" >> pure Call) <|>
    (keyword "Return" >> pure Return)
  
- let rec parse_coms () =
+let rec parse_coms () =
    many (parse_com parse_coms << keyword ";")
  
 
@@ -119,6 +126,7 @@ let parse_const =
 
 type stack = const list
 type trace = string list
+type env = (string * const) list
 type prog = coms
 
 let rec str_of_nat (n : int) : string =
@@ -141,73 +149,76 @@ let toString (c : const) : string =
   | Bool false -> "False"
   | Unit -> "Unit"
 
-let rec eval (s : stack) (t : trace) (p : prog) : trace =
+let rec eval (s : stack) (t : trace) (v: env) (p : prog) : trace =
   match p with
   (* termination state returns the trace *)
   | [] -> t
-  | Push c :: p0 (* PushStack *) -> eval (c :: s) t p0
+  | Push c :: p0 (* PushStack *) -> eval (c :: s) t v p0
   | Pop :: p0 ->
     (match s with
-     | _ :: s0 (* PopStack *) -> eval s0 t p0
-     | []      (* PopError *) -> eval [] ("Panic" :: t) [])
+     | _ :: s0 (* PopStack *) -> eval s0 t v p0
+     | []      (* PopError *) -> eval [] ("Panic" :: t) [] [])
   | Trace :: p0 ->
     (match s with
-     | c :: s0 (* TraceStack *) -> eval (Unit :: s0) (toString c :: t) p0
-     | []      (* TraceError *) -> eval [] ("Panic" :: t) [])
+     | c :: s0 (* TraceStack *) -> eval (Unit :: s0) (toString c :: t) v p0
+     | []      (* TraceError *) -> eval [] ("Panic" :: t) [] [])
   | Add :: p0 ->
     (match s with
-     | Int i :: Int j :: s0 (* AddStack *)  -> eval (Int (i + j) :: s0) t p0
-     | _ :: _ :: s0         (* AddError1 *) -> eval [] ("Panic" :: t) []
-     | []                   (* AddError2 *) -> eval [] ("Panic" :: t) []
-     | _ :: []              (* AddError3 *) -> eval [] ("Panic" :: t) [])
+     | Int i :: Int j :: s0 (* AddStack *)  -> eval (Int (i + j) :: s0) t v p0
+     | _ :: _ :: s0         (* AddError1 *) -> eval [] ("Panic" :: t) [] []
+     | []                   (* AddError2 *) -> eval [] ("Panic" :: t) [] []
+     | _ :: []              (* AddError3 *) -> eval [] ("Panic" :: t) [] [])
   | Sub :: p0 ->
     (match s with
-     | Int i :: Int j :: s0 (* SubStack *)  -> eval (Int (i - j) :: s0) t p0
-     | _ :: _ :: s0         (* SubError1 *) -> eval [] ("Panic" :: t) []
-     | []                   (* SubError2 *) -> eval [] ("Panic" :: t) []
-     | _ :: []              (* SubError3 *) -> eval [] ("Panic" :: t) [])
+     | Int i :: Int j :: s0 (* SubStack *)  -> eval (Int (i - j) :: s0) t v p0
+     | _ :: _ :: s0         (* SubError1 *) -> eval [] ("Panic" :: t) [] []
+     | []                   (* SubError2 *) -> eval [] ("Panic" :: t) [] []
+     | _ :: []              (* SubError3 *) -> eval [] ("Panic" :: t) [] [])
   | Mul :: p0 ->
     (match s with
-     | Int i :: Int j :: s0 (* MulStack *)  -> eval (Int (i * j) :: s0) t p0
-     | _ :: _ :: s0         (* MulError1 *) -> eval [] ("Panic" :: t) []
-     | []                   (* MulError2 *) -> eval [] ("Panic" :: t) []
-     | _ :: []              (* MulError3 *) -> eval [] ("Panic" :: t) [])
+     | Int i :: Int j :: s0 (* MulStack *)  -> eval (Int (i * j) :: s0) t v p0
+     | _ :: _ :: s0         (* MulError1 *) -> eval [] ("Panic" :: t) [] []
+     | []                   (* MulError2 *) -> eval [] ("Panic" :: t) [] []
+     | _ :: []              (* MulError3 *) -> eval [] ("Panic" :: t) [] [])
   | Div :: p0 ->
     (match s with
-     | Int i :: Int 0 :: s0 (* DivError0 *) -> eval [] ("Panic" :: t) []
-     | Int i :: Int j :: s0 (* DivStack *)  -> eval (Int (i / j) :: s0) t p0
-     | _ :: _ :: s0         (* DivError1 *) -> eval [] ("Panic" :: t) []
-     | []                   (* DivError2 *) -> eval [] ("Panic" :: t) []
-     | _ :: []              (* DivError3 *) -> eval [] ("Panic" :: t) [])
+     | Int i :: Int 0 :: s0 (* DivError0 *) -> eval [] ("Panic" :: t) [] []
+     | Int i :: Int j :: s0 (* DivStack *)  -> eval (Int (i / j) :: s0) t v p0
+     | _ :: _ :: s0         (* DivError1 *) -> eval [] ("Panic" :: t) [] []
+     | []                   (* DivError2 *) -> eval [] ("Panic" :: t) [] []
+     | _ :: []              (* DivError3 *) -> eval [] ("Panic" :: t) [] [])
   | And :: p0 ->
     (match s with
-     | Bool a :: Bool b :: s0 (* AndStack *)  -> eval (Bool (a && b) :: s0) t p0
-     | _ :: _ :: s0           (* AndError1 *) -> eval [] ("Panic" :: t) []
-     | []                     (* AndError2 *) -> eval [] ("Panic" :: t) []
-     | _ :: []                (* AndError3 *) -> eval [] ("Panic" :: t) [])
+     | Bool a :: Bool b :: s0 (* AndStack *)  -> eval (Bool (a && b) :: s0) t v p0
+     | _ :: _ :: s0           (* AndError1 *) -> eval [] ("Panic" :: t) [] []
+     | []                     (* AndError2 *) -> eval [] ("Panic" :: t) [] []
+     | _ :: []                (* AndError3 *) -> eval [] ("Panic" :: t) [] [])
   | Or :: p0 ->
     (match s with
-     | Bool a :: Bool b :: s0 (* OrStack *)  -> eval (Bool (a || b) :: s0) t p0
-     | _ :: _ :: s0           (* OrError1 *) -> eval [] ("Panic" :: t) []
-     | []                     (* OrError2 *) -> eval [] ("Panic" :: t) []
-     | _ :: []                (* OrError3 *) -> eval [] ("Panic" :: t) [])
+     | Bool a :: Bool b :: s0 (* OrStack *)  -> eval (Bool (a || b) :: s0) t v p0
+     | _ :: _ :: s0           (* OrError1 *) -> eval [] ("Panic" :: t) [] []
+     | []                     (* OrError2 *) -> eval [] ("Panic" :: t) [] []
+     | _ :: []                (* OrError3 *) -> eval [] ("Panic" :: t) [] [])
   | Not :: p0 ->
     (match s with
-     | Bool a :: s0 (* NotStack  *) -> eval (Bool (not a) :: s0) t p0
-     | _ :: s0      (* NotError1 *) -> eval [] ("Panic" :: t) []
-     | []           (* NotError2 *) -> eval [] ("Panic" :: t) [])
+     | Bool a :: s0 (* NotStack  *) -> eval (Bool (not a) :: s0) t v p0
+     | _ :: s0      (* NotError1 *) -> eval [] ("Panic" :: t) [] []
+     | []           (* NotError2 *) -> eval [] ("Panic" :: t) [] [])
   | Lt :: p0 ->
     (match s with
-     | Int i :: Int j :: s0 (* LtStack *)  -> eval (Bool (i < j) :: s0) t p0
-     | _ :: _ :: s0         (* LtError1 *) -> eval [] ("Panic" :: t) []
-     | []                   (* LtError2 *) -> eval [] ("Panic" :: t) []
-     | _ :: []              (* LtError3 *) -> eval [] ("Panic" :: t) [])
+     | Int i :: Int j :: s0 (* LtStack *)  -> eval (Bool (i < j) :: s0) t v p0
+     | _ :: _ :: s0         (* LtError1 *) -> eval [] ("Panic" :: t) [] []
+     | []                   (* LtError2 *) -> eval [] ("Panic" :: t) [] []
+     | _ :: []              (* LtError3 *) -> eval [] ("Panic" :: t) [] [])
   | Gt :: p0 ->
     (match s with
-     | Int i :: Int j :: s0 (* GtStack *)  -> eval (Bool (i > j) :: s0) t p0
-     | _ :: _ :: s0         (* GtError1 *) -> eval [] ("Panic" :: t) []
-     | []                   (* GtError2 *) -> eval [] ("Panic" :: t) []
-     | _ :: []              (* GtError3 *) -> eval [] ("Panic" :: t) [])
+     | Int i :: Int j :: s0 (* GtStack *)  -> eval (Bool (i > j) :: s0) t v p0
+     | _ :: _ :: s0         (* GtError1 *) -> eval [] ("Panic" :: t) [] []
+     | []                   (* GtError2 *) -> eval [] ("Panic" :: t) [] []
+     | _ :: []              (* GtError3 *) -> eval [] ("Panic" :: t) [] [])
+
+   
+    
 
 (* ------------------------------------------------------------ *)
 
@@ -215,7 +226,7 @@ let rec eval (s : stack) (t : trace) (p : prog) : trace =
 
 let interp (s : string) : string list option =
   match string_parse (whitespaces >> parse_coms ()) s with
-  | Some (p, []) -> Some (eval [] [] p)
+  | Some (p, []) -> Some (eval [] [] [] p)
   | _ -> None
 
 (* ------------------------------------------------------------ *)
@@ -239,3 +250,11 @@ let interp_file (fname : string) : string list option =
 
 
 (* ------------------------------------------------------------ *)
+let test () =
+   let test_program = 
+   "Push -2; Trace;" (* Placeholder for test input string *)
+   in match interp test_program with
+      | Some trace -> List.iter print_endline trace
+      | None -> print_endline "Program failed to interpret"
+ 
+ let _ = test()
